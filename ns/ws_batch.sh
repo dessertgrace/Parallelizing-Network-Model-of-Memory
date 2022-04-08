9111b9b28faeabccb87d963751f77a345d55ab19
Minhaj Hussain <mah148@duke.edu>
2022-04-08 16:37:37 -0400

    batch script

--- ns/ws_batch.sh
+++ ns/ws_batch.sh
@@ -3,6 +3,7 @@
 #SBATCH --job-name=ws_array         # Job name
 #SBATCH --nodes=1                   # Use one node
 #SBATCH --ntasks=1                  # Run a single task
+#SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=16gb          # Memory per processor
 #SBATCH --time=08:00:00             # Time limit hrs:min:sec
 #SBATCH --output=ws_rray_%A-%a.out  # Standard output and error log
