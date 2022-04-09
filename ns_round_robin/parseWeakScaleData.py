36d8c3b8a413ce3d1d298cfad1fa0c290df95a37
Grace Dessert <ged12@login1.stampede2.tacc.utexas.edu>
2022-04-09 14:27:30 -0500

    add SS and WS files to round_robin ns

---
+++ ns_round_robin/parseWeakScaleData.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# Grace Dessert, April 7 2022
+# BME 590L Dr. Randles
+# parse weak scale data and create plots
+# NOTE based on how the outputs are extracted, only run one simulation at a time! 
+
+import os
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+
+def main():
+    # weak scale cases
+    numDirs = [4, 8, 12, 16, 20, 24, 36, 64, 128, 256]
+
+    # open last X directories in out directory
+    dirsInOut = os.listdir('out/')
+    dirsInOut.sort()
+    print(dirsInOut)
+    # plot out file
+    plotFile = "weakScalePlot.svg"
+
+    # for each get time from file '0_0.raw'
+    maxTimes = []
+    for i in range(len(numDirs)):
+	propDir = os.listdir('out/'+dirsInOut[-i-1])
+        fileName = "out/" + dirsInOut[-i-1] + "/" + propDir[0] + "/0_0.raw"
+	print("Opening file: ", fileName)
+        with open(fileName, 'r') as file:
+            for line in file:
+                if line.startswith("timing"):
+                    last_line = line.split(" ")
+                    maxTime = float(last_line[1])
+                    maxTimes.append(maxTime)
+                    break
+    maxTimes.reverse()
+
+    if len(maxTimes) != len(numDirs):
+        print('ERROR: number of outputs found is not equal to number of runs expected')
+
+    # plot and save plot
+    plt.figure(figsize=[9, 5])
+    plt.grid(1, axis='y')
+    plt.plot(numDirs, maxTimes, '-o')
+
+    # legendEntries = ["Baseline", "HPC lesion", "ACC lesion"]
+    # plt.legend(legendEntries, loc='best')
+
+    plt.ylabel("Max Rank Runtime")
+    plt.xlabel("Num Processors (with problem size scaling linearly)")
+    plt.title("Weak Scaling")
+    # plt.show()
+    plt.savefig(plotFile)
+
+
+if __name__ == "__main__":
+    main()
