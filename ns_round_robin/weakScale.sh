b10a6c1041006bddc49bf0bf391d333627f8d530
Grace Dessert <ged12@login4.stampede2.tacc.utexas.edu>
2022-04-11 09:29:42 -0500

    fix scaling plots for RR parallel case and add some scaling data

--- ns_round_robin/weakScale.sh
+++ ns_round_robin/weakScale.sh
@@ -8,16 +8,16 @@
 #SBATCH -p normal
 #SBATCH -A TG-MDE210001
 
-./multi_ns_parallel -p b_04 -t FATAL --caseID=ws -f -n 4 1
-./multi_ns_parallel -p b_08 -t FATAL --caseID=ws -f -n 8 1
-./multi_ns_parallel -p b_12 -t FATAL --caseID=ws -f -n 12 1
-./multi_ns_parallel -p b_16 -t FATAL --caseID=ws -f -n 16 1
-./multi_ns_parallel -p b_20 -t FATAL --caseID=ws -f -n 20 1
-./multi_ns_parallel -p b_24 -t FATAL --caseID=ws -f -n 24 1
-./multi_ns_parallel -p b_36 -t FATAL --caseID=ws -f -n 36 1
-./multi_ns_parallel -p b_64 -t FATAL --caseID=ws -f -n 64 1
-./multi_ns_parallel -p b_128 -t FATAL --caseID=ws -f -n 128 1
-./multi_ns_parallel -p b_256 -t FATAL --caseID=ws -f -n 256 1
+#./multi_ns_parallel -p b_04 -t FATAL --caseID=ws -f -n 4 1
+#./multi_ns_parallel -p b_08 -t FATAL --caseID=ws -f -n 8 1
+#./multi_ns_parallel -p b_12 -t FATAL --caseID=ws -f -n 12 1
+#./multi_ns_parallel -p b_16 -t FATAL --caseID=ws -f -n 16 1
+#./multi_ns_parallel -p b_20 -t FATAL --caseID=ws -f -n 20 1
+#./multi_ns_parallel -p b_24 -t FATAL --caseID=ws -f -n 24 1
+#./multi_ns_parallel -p b_36 -t FATAL --caseID=ws -f -n 36 1
+#./multi_ns_parallel -p b_64 -t FATAL --caseID=ws -f -n 64 1
+#./multi_ns_parallel -p b_128 -t FATAL --caseID=ws -f -n 128 1
+#./multi_ns_parallel -p b_256 -t FATAL --caseID=ws -f -n 256 1
 
 # submit python parse output script to get timings from output files
 # to open correct files, get last X directories in the out directory in order and parse total times from 0_0.raw end
