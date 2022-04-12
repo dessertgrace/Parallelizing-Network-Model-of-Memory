28c2eae16c3c79cee5153955a5bca077bd4cbdc5
Grace Dessert <grace.dessert@duke.edu>
2022-04-11 20:59:57 -0400

    titles and jpegs SS plots

--- makePlots.py
+++ makePlots.py
@@ -20,11 +20,11 @@ def main():
     # plt.grid(1, axis='y')
     plt.grid(True)
     plt.yscale('log')
-    plt.xscale('log')
+    # plt.xscale('log')
     plt.plot(numDirs, roundRobinSS, '-o')
     plt.plot(numDirs, layerBasedSS, '-o')
-    # xTicks = [4, 16, 36, 64, 128, 256]
-    plt.xticks(numDirs, numDirs)
+    xTicks = [4, 16, 36, 64, 128, 256]
+    plt.xticks(xTicks, xTicks)
     yTicks = [10,25,50,100,200,400,800,1600]
     # yTicks = [10, 200, 400, 1000, 1600]
     plt.yticks(yTicks, yTicks)  # np.arange(1, round(max(layerBasedSS)/10)*10, round(max(layerBasedSS)/10)*10/10)
@@ -34,8 +34,10 @@ def main():
 
     plt.ylabel("Max Rank Runtime (secs)")
     plt.xlabel("Num Processors")
+    plt.title("Strong Scaling of 25x25 Network Simulation")
     # plt.show()
-    plt.savefig("strongScalingCombined_logLog.svg")
+    plt.savefig("strongScalingCombined_logY.svg")
+    plt.savefig("strongScalingCombined_logY.jpeg")
 
     # with open('strongScaleData.pkl', 'wb') as f:
     #     pickle.dump(maxTimes, f)
